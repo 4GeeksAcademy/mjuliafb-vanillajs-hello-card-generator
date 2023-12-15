602c80362089ff47887a46abb5f6e19e3ee0aada
Maria Julia Fernandez Balice (MJ) <136009647+Mjuliafb@users.noreply.github.com>
2023-12-15 14:54:09 +0000

    second commit button add it

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector("button").addEventListener("click", function() {
+    document.querySelector("#totalCard").innerHTML = generateCard();
+  });
+
+  document.querySelector("#totalCard").innerHTML = generateCard();
+});
+
+const generateCard = () => {
+  function type() {
+    let cardType = ["♣️", "♦️", "♥️", "♠️"];
+    let cardIndex = Math.floor(Math.random() * cardType.length);
+    return cardType[cardIndex];
+  }
+
+  function number() {
+    let numberIndex = Math.floor(Math.random() * 9) + 2;
+    return numberIndex;
+  }
+
+  let allType = type();
+  let cardTopColor = allType === "♥️" ? "red" : "black";
+
+  return `
+    <div class="cardTop">
+      <p class="cardTypeUp" style="color: ${cardTopColor}">${allType}</p>
+    </div>
+    <div class="cardCenter">
+      <p class="cardNumber">${number()}</p>
+    </div>
+    <div class="cardBottom">
+      <p class="cardTypeBottom" style="color: ${cardTopColor}">${allType}</p>
+    </div>
+  `;
 };
